6643d13a3ef63e0d23de62cb0f91cc68d89eab76
kaneezfatima428 <hafizakaneezfatima.04@gmail.com>
2025-12-22 02:28:09 +0500

    assignment

---
+++ chap1 assignment/app.js
@@ -0,0 +1,6 @@
+alert("السلام وعلیکم")
+alert("Error!Please enter a valid password.")
+alert("Welcome to js land \n Happy coding.")
+alert("welcome to js land...")
+alert("Happy Coding!\n Pevent this page from Creating additional dialoges")
+alert("Hello... I can run JS through my web browser's console");
---
+++ chap2 assignment/app.js
@@ -0,0 +1,33 @@
+var username;
+
+    var myName = "kaneezfatima";  
+
+    var message = "Hello World";
+    alert(message);
+
+    var studentName = "Jhone Doe";
+    var studentAge = "15 Years Old";
+    var studentCourse ="  Certified Mobile Application Development";
+
+    alert(studentName);
+    alert(studentAge);
+    alert(studentCourse);
+   
+
+    
+    var pizzaMsg = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+    alert(pizzaMsg);
+
+    var email = "hafizkaneezfatima.04@gmail.com";  
+    alert("my email address is hafizkaneezfatima.04@gmail.com");
+
+    
+    var book = "I am trying to learn from the book A smarter way to learn JavaScript";
+    alert("I am trying to learn from the book A smarter way to learn JavaScript");
+
+    document.write("Yah! I can write HTML content through JavaScript");
+
+    var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+    alert(specialString);
+    
